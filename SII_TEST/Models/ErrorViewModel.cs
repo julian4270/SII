d1c4f015e1c11305e12358bad60bae3027483d35
JULIAN JOSE CASTELLANOS URIBE <jcastellanos@mnatg.com>
2023-06-27 11:23:17 -0500

    commit initial
    commit initial

---
+++ SII_TEST/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SII_TEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
